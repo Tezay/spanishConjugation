bf04b76747ec65d92ab217a1b43f0f3b550cec7d
Ceyrocx <98047734+Ceyrocx@users.noreply.github.com>
2023-03-13 22:02:06 +0100

    Add files via upload

---
+++ apelCSV.py
@@ -0,0 +1,76 @@
+from random import*
+import csv
+
+
+def verbChoice():
+    
+    with open('data.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = [row for row in reader]
+        random_verbe = random.choice(rows)[0]
+        
+        return random_verbe
+
+def Presente_de_indicativo():
+    
+    with open('times/Presente de indicativo.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        rows = [row for row in reader]
+                
+        return rows
+
+
+def Presente_de_subjonctivo():
+    
+    with open('times/Presente de subjonctivo.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        rows = [row for row in reader]
+                
+        return rows
+
+    
+def Preterito_imperfecto_de_indicativo():
+    
+    with open('times/Pretérito imperfecto de indicativo.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        rows = [row for row in reader]
+        
+        return rows
+
+
+def Preterito_indefinido():
+    
+    with open('times/Pretérito indefinido.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        rows = [row for row in reader]
+                
+        return rows
+
+
+def Pretero_imperfecto_de_subjonctivo():
+    
+    with open('times/Prétero imperfecto de subjonctivo.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        rows = [row for row in reader]
+        
+        return rows
+
+def Futuro():
+    
+    with open('times/Futuro.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        rows = [row for row in reader]
+        
+        return rows
+
+
+def Conditional():
+    
+    with open('times/Conditional.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        rows = [row for row in reader]
+        
+        return rows
+
+print(Futuro())    
+    
