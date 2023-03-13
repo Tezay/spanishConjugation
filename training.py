f3fab2f30c34bc09fc2b160c55a05abce39e5a0f
Tezay <69462822+Tezay@users.noreply.github.com>
2023-03-13 22:55:03 +0100

    pass

--- training.py
+++ training.py
@@ -18,37 +18,31 @@ def userInput(verb, time, pronouns):
 
 
 
-def verification(response, verb, time, pronouns):
+def verification(response, verb, time, pronouns): #time = "Conditional" (par exemple)
     
     termination = str(verb[-2:])
     
     correspondanceTermination = ["ar","er","ir"]
 
     correspondanceTime = {
-        "Conditional": Conditional(),
-        "Futuro": Futuro(),
-        "Presente de indicativo": Presente_de_indicativo(),
-        "Presente de subjonctivo": Presente_de_subjonctivo(),
-        "Pretérito imperfecto de indicativo": Preterito_imperfecto_de_indicativo(),
-        "Pretérito indefinido": Preterito_indefinido(),
-        "Prétero imperfecto de subjonctivo": Pretero_imperfecto_de_subjonctivo(),
+        "Conditional": Conditional,
+        "Futuro": Futuro,
+        "Presente de indicativo": Presente_de_indicativo,
+        "Presente de subjonctivo": Presente_de_subjonctivo,
+        "Pretérito imperfecto de indicativo": Preterito_imperfecto_de_indicativo,
+        "Pretérito indefinido": Preterito_indefinido,
+        "Prétero imperfecto de subjonctivo": Pretero_imperfecto_de_subjonctivo,
     }
 
-    if time == "Imparfait":
-        indice1 = 0
-        
-    else:
-        indice1 = 1
+
         
     
-    if response == verb[:-2] + csvReader.time[indice1][indice2][int(personnes.index(pronouns))]:
+    if response == verb[:-2] + csvReader.correspondanceTime[time]()[indice1][indice2][int(personnes.index(pronouns))]:
         
         print("✅")
         
     else:
-        print("\n❌ Answer was: ", verb[:-2] + csvReader.time[indice1][indice2][int(personnes.index(pronouns))])
-
-
+        print("\n❌ Answer was: ", verb[:-2] + csvReader.correspondanceTime[time]()[indice1][indice2][int(personnes.index(pronouns))])
 
 
 
