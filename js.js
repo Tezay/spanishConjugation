8926bad48554fce6b06d630c321c2d74bc18c642
Tezay <69462822+Tezay@users.noreply.github.com>
2023-03-29 22:16:55 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+document.getElementById("inscription").addEventListener("submit", fonction(e) {
+		
+	e.preventDefault();
+		
+	var inputs = document.getElementsByTagName("input");
+		
+	alert("banane");
+	return false;
+
+});
+		
+		
+		
